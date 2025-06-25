aff5f411e9b71a531e720a585939f1d426b0c2c0
shmuelgross <shmuelgross3@gmail.com>
2025-06-25 11:06:22 +0300

    create shape

--- shape.py
+++ shape.py
@@ -1,1 +1,19 @@
-print("shmuel")
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    def __init__(self, name="Shape"):
+        self.name = name
+
+    @abstractmethod
+    def get_area(self):
+        pass
+
+    @abstractmethod
+    def get_perimeter(self):
+        pass
+
+    def __str__(self):
+        return f"{self.name} | Area: {self.get_area():.2f} | Perimeter: {self.get_perimeter():.2f}"
+
+    def __repr__(self):
+        return str(self)
